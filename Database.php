b0ee64e7c34359ea7fba7346393b2d7a4e2e87c9
CodyJReed <cody_j_reed@proton.me>
2026-05-23 17:16:19 -0400

    clean up; add  to PDO creation; add public query method to db class

--- Database.php
+++ Database.php
@@ -13,13 +13,33 @@ class Database {
         $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
 
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
 
         try{
-            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $err) {
             throw new Exception(("Database connection failed: {$err->getMessage()}"));
+        }
+    }
+
+    /**
+     * Query DB
+     * 
+     * @param string $query
+     * 
+     * @return PDOStatement
+     * @throws PDOException
+     */
+    public function query($query) {
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+
+            return $stmt;
+        } catch (PDOException $err) {
+            throw new Exception("Query failed to execute: {$err->getMessage()}");
         }
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,14 +2,13 @@
 require '../helpers.php';
 require basePath('Router.php');
 require basePath('Database.php');
-$config = require basePath('config/db.php');
 
-$db = new Database($config);
-
+// Create router instance
 $router = new Router();
+// Retrieve routes
 $routes = require basePath('routes.php');
-
+// Get current uri and http method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
-
+// Route reques(s)
 $router->route($uri, $method);
