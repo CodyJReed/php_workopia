c7710cb0bfed02ac2754124f6461cd080e0972d0
CodyJReed <cody_j_reed@proton.me>
2026-05-24 15:25:31 -0400

    wire 'show' page for displaying individual listings

--- public/index.php
+++ public/index.php
@@ -7,8 +7,8 @@ require basePath('Database.php');
 $router = new Router();
 // Retrieve routes
 $routes = require basePath('routes.php');
-// Get current uri and http method
-$uri = $_SERVER['REQUEST_URI'];
+// Get current uri and http method.. stripping out query params
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 // Route reques(s)
 $router->route($uri, $method);
